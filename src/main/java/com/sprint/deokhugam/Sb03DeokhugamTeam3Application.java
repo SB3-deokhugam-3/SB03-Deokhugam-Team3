a36d107a8f9b0ed4bbbaf05d7ba61b23f5e4c833
LZHTK <gusdnjsrl2@gmail.com>
2025-07-08 22:03:30 +0900

    init: 프로젝트 시작

---
+++ src/main/java/com/sprint/deokhugam/Sb03DeokhugamTeam3Application.java
@@ -0,0 +1,13 @@
+package com.sprint.deokhugam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb03DeokhugamTeam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb03DeokhugamTeam3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprint/deokhugam/Sb03DeokhugamTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.deokhugam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb03DeokhugamTeam3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
