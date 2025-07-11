3bbfb2353dcc1eca3343ade6c447ad5e47260a61
barrackkk <kgmlwns904@gmail.com>
2025-07-11 16:53:14 +0900

    test: 중복된 이메일로 회원가입 시 실패하는 테스트 추가

--- src/test/java/com/sprint/deokhugam/domain/user/service/UserServiceImplTest.java
+++ src/test/java/com/sprint/deokhugam/domain/user/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.sprint.deokhugam.domain.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -53,4 +54,18 @@ class UserServiceImplTest {
         assertThat(result.email()).isEqualTo("userEmail@test.com");
     }
 
+    @Test
+    void 중복_이메일로_회원가입_시도_시_회원가입을_실패한다() {
+        // given
+        UserCreateRequest request = new UserCreateRequest("duplicate@example.com", "testuser",
+                "password");
+
+        //request.email 존재한다고 가정 ->  repo true 반환
+        given(userRepository.existsByEmail(request.email())).willReturn(true);
+
+        // when & then
+        assertThatThrownBy(() -> userService.createUser(request))
+                .hasMessageContaining("이미 존재하는 이메일입니다.");
+    }
 }
+
