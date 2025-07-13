3906df91befd91fe039c48fd44f0a35d33ac60e9
yeokyeong <yeokyeong1004@gmail.com>
2025-07-13 21:31:39 +0900

    refactor: review 엔티티, reviewDto, reviewRequest 빌더 어노테이션 추가

--- src/main/java/com/sprint/deokhugam/domain/review/dto/request/ReviewRequest.java
+++ src/main/java/com/sprint/deokhugam/domain/review/dto/request/ReviewRequest.java
@@ -2,18 +2,20 @@ package com.sprint.deokhugam.domain.review.dto.request;
 
 import java.time.Instant;
 import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
 
-public record ReviewRequest(
-    UUID userId, // 작성자 ID
-    UUID bookId, // 도서 ID
-    String keyword, // 검색 키워드 (작성자 닉네임 | 내용)
-    String orderBy, // 정렬 기준(createdAt | rating)
-    String direction, // 정렬 방향( DESC | ASC )
-    String cursor, //  페이지네이션 커서
-    Instant after, // 보조 커서(createdAt)
-    Integer limit, // 페이지 크기
-    UUID requestUserId // 요청자 ID , not null
-// 추가)header에 Deokhugam-Request-User-ID 넘어옴
-) {
+@Getter
+@Builder(toBuilder = true)
+public class ReviewRequest {
 
+    private UUID userId; // 작성자 ID
+    private UUID bookId; // 도서 ID
+    private String keyword; // 검색 키워드 (작성자 닉네임 | 내용)
+    private String orderBy; // 정렬 기준(createdAt | rating)
+    private String direction; // 정렬 방향( DESC | ASC )
+    private Object cursor; //  페이지네이션 커서
+    private Instant after; // 보조 커서(createdAt)
+    private Integer limit = 50; // 페이지 크기 , default:50
+    private UUID requestUserId; // 요청자 ID , not null
 }
