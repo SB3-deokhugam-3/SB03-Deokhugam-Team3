d1d879113dc6649c26da9190fd08dbc96ccc59d6
yeokyeong <yeokyeong1004@gmail.com>
2025-07-14 19:50:48 +0900

    fix: review 엔티티에 builder 추가

--- src/main/java/com/sprint/deokhugam/domain/review/entity/Review.java
+++ src/main/java/com/sprint/deokhugam/domain/review/entity/Review.java
@@ -10,12 +10,16 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "reviews")
+@AllArgsConstructor
+@Builder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SQLRestriction("is_deleted = false")
