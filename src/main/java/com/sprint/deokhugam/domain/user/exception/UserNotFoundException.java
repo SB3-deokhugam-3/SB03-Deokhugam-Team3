85fdbecd8d84e1dcf21d0c112363263909d1ad9d
barrackkk <kgmlwns904@gmail.com>
2025-07-14 18:32:58 +0900

    refactor: UserNotFoundException에 email 불일치시 관련 코드 추가

--- src/main/java/com/sprint/deokhugam/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/sprint/deokhugam/domain/user/exception/UserNotFoundException.java
@@ -2,12 +2,18 @@ package com.sprint.deokhugam.domain.user.exception;
 
 import com.sprint.deokhugam.global.exception.NotFoundException;
 import java.util.Map;
+import java.util.UUID;
 
 public class UserNotFoundException extends NotFoundException {
     private final String message;
 
-    public UserNotFoundException(String userId, String message) {
+    public UserNotFoundException(UUID userId, String message) {
         super("USER", Map.of("userId", userId));
+        this.message = message;
+    }
+
+    public UserNotFoundException(String email, String message) {
+        super("USER", Map.of("email", email));
         this.message = message;
     }
 
