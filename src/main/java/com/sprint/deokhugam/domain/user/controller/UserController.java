6410288b565ce663389037ce69e48e94dd2000e8
barrackkk <kgmlwns904@gmail.com>
2025-07-14 23:09:59 +0900

    feat: 사용자 로그인 시 Post요청 구현

--- src/main/java/com/sprint/deokhugam/domain/user/controller/UserController.java
+++ src/main/java/com/sprint/deokhugam/domain/user/controller/UserController.java
@@ -2,10 +2,12 @@ package com.sprint.deokhugam.domain.user.controller;
 
 import com.sprint.deokhugam.domain.user.dto.data.UserDto;
 import com.sprint.deokhugam.domain.user.dto.request.UserCreateRequest;
+import com.sprint.deokhugam.domain.user.dto.request.UserLoginRequest;
 import com.sprint.deokhugam.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -41,5 +44,16 @@ public class UserController {
     ) {
         UserDto user = userService.findUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDto> login(
+            @Valid @RequestBody UserLoginRequest userLoginRequest
+    ) {
+        UserDto loginUser = userService.loginUser(userLoginRequest);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(loginUser);
     }
 }
