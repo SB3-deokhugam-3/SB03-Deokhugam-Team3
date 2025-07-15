e3d4bd9111d9d4f10ba5504d34edb99fa0f21930
hyohyo-zz <akbkck8101@gmail.com>
2025-07-15 20:53:33 +0900

    feat: 리뷰 수정 controller 구현 - 권한 커스텀예외 추가, 403 응답으로 변환

--- src/main/java/com/sprint/deokhugam/domain/review/exception/ReviewUnauthorizedAccessException.java
+++ src/main/java/com/sprint/deokhugam/domain/review/exception/ReviewUnauthorizedAccessException.java
@@ -1,10 +1,10 @@
 package com.sprint.deokhugam.domain.review.exception;
 
-import com.sprint.deokhugam.global.exception.UnauthorizedException;
+import com.sprint.deokhugam.global.exception.ForbiddenException;
 import java.util.Map;
 import java.util.UUID;
 
-public class ReviewUnauthorizedAccessException extends UnauthorizedException {
+public class ReviewUnauthorizedAccessException extends ForbiddenException {
 
     public ReviewUnauthorizedAccessException(UUID reviewId, UUID userId) {
         super("Review", Map.of("reviewId", reviewId, "userId", userId));
