a5c7af94ed69ef79d343b691b57b7306644f05d7
yeokyeong <yeokyeong1004@gmail.com>
2025-07-16 16:08:52 +0900

    feat: 댓글 수정 기능 api

--- src/main/java/com/sprint/deokhugam/domain/comment/service/CommentService.java
+++ src/main/java/com/sprint/deokhugam/domain/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package com.sprint.deokhugam.domain.comment.service;
 
 import com.sprint.deokhugam.domain.comment.dto.data.CommentDto;
 import com.sprint.deokhugam.domain.comment.dto.request.CommentCreateRequest;
+import com.sprint.deokhugam.domain.comment.dto.request.CommentUpdateRequest;
 import java.util.UUID;
 
 public interface CommentService {
@@ -9,4 +10,6 @@ public interface CommentService {
     CommentDto create(CommentCreateRequest request);
 
     CommentDto findById(UUID commentId);
+
+    CommentDto updateById(UUID commentId, CommentUpdateRequest request, UUID requestUserId);
 }
