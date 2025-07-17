5a0cf3c47d4045d3a77c8bee65b80ee657015835
barrackkk <kgmlwns904@gmail.com>
2025-07-17 09:16:44 +0900

    feat: 알림 Repository 구현

--- src/main/java/com/sprint/deokhugam/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/sprint/deokhugam/domain/notification/repository/NotificationRepository.java
@@ -1,4 +1,10 @@
 package com.sprint.deokhugam.domain.notification.repository;
 
-public class NotificationRepository {
+import com.sprint.deokhugam.domain.notification.entity.Notification;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID>, NotificationRepositoryCustom {
 }
