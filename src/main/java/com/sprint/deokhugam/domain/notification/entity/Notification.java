f5e0eda3ed1c92fbdd80fa7e1989a6bae73ad217
barrackkk <kgmlwns904@gmail.com>
2025-07-17 09:15:46 +0900

    feat: 알림 엔티티 설계

--- src/main/java/com/sprint/deokhugam/domain/notification/entity/Notification.java
+++ src/main/java/com/sprint/deokhugam/domain/notification/entity/Notification.java
@@ -1,4 +1,41 @@
 package com.sprint.deokhugam.domain.notification.entity;
 
-public class Notification {
+import com.sprint.deokhugam.domain.review.entity.Review;
+import com.sprint.deokhugam.domain.user.entity.User;
+import com.sprint.deokhugam.global.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "notifications")
+public class Notification extends BaseUpdatableEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(length = 255)
+    private String content;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private boolean isConfirmed = false;
+
+    public void confirm() {
+        this.isConfirmed = true;
+    }
 }
