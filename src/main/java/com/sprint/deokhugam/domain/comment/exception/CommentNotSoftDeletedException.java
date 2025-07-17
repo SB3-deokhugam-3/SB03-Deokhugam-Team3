ff7db49bcd85390271ea874664ce07dd6046f9af
hyohyo-zz <akbkck8101@gmail.com>
2025-07-18 02:18:19 +0900

    feat: 논리삭제 되지 않은 댓글에 대한 예외 상속 계층 BadRequestException으로 변경

--- src/main/java/com/sprint/deokhugam/domain/comment/exception/CommentNotSoftDeletedException.java
+++ src/main/java/com/sprint/deokhugam/domain/comment/exception/CommentNotSoftDeletedException.java
@@ -1,10 +1,10 @@
 package com.sprint.deokhugam.domain.comment.exception;
 
-import com.sprint.deokhugam.global.exception.NotFoundException;
+import com.sprint.deokhugam.global.exception.BadRequestException;
 import java.util.Map;
 import java.util.UUID;
 
-public class CommentNotSoftDeletedException extends NotFoundException {
+public class CommentNotSoftDeletedException extends BadRequestException {
 
     public CommentNotSoftDeletedException(UUID commentId) {
         super("comment", Map.of("commentId", commentId, "message", "논리 삭제되지 않은 댓글입니다."));
