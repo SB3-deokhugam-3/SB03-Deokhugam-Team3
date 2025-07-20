484323ce5a5b8e838bf9a4baeef8e7cc8ae1b561
Dw-real <gksehddn8@naver.com>
2025-07-20 23:11:10 +0900

    feat: PeriodType 랭킹 기준 날짜 로직 구현

--- src/main/java/com/sprint/deokhugam/global/period/PeriodType.java
+++ src/main/java/com/sprint/deokhugam/global/period/PeriodType.java
@@ -1,29 +1,67 @@
 package com.sprint.deokhugam.global.period;
 
+import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
-import org.springframework.cglib.core.Local;
+import java.time.ZoneId;
 
 public enum PeriodType {
     DAILY {
-        public LocalDate getStartDate(LocalDate baseDate) {
-            return baseDate;
+        @Override
+        public Instant getStartInstant(Instant today, ZoneId zone) {
+            LocalDate date = LocalDate.ofInstant(today, zone);
+            return date.atStartOfDay(zone).toInstant();
+        }
+
+        @Override
+        public Instant getEndInstant(Instant today, ZoneId zone) {
+            LocalDate date = LocalDate.ofInstant(today, zone).plusDays(1);
+            return date.atStartOfDay(zone).toInstant();
         }
     },
     WEEKLY {
-        public LocalDate getStartDate(LocalDate baseDate) {
-            return baseDate.minusDays(6);
+        @Override
+        public Instant getStartInstant(Instant today, ZoneId zone) {
+            LocalDate date = LocalDate.ofInstant(today, zone)
+                .with(DayOfWeek.MONDAY);
+            return date.atStartOfDay(zone).toInstant();
+        }
+
+        @Override
+        public Instant getEndInstant(Instant today, ZoneId zone) {
+            LocalDate date = LocalDate.ofInstant(today, zone)
+                .with(DayOfWeek.MONDAY).plusWeeks(1);
+            return date.atStartOfDay(zone).toInstant();
         }
     },
     MONTHLY {
-        public LocalDate getStartDate(LocalDate baseDate) {
-            return baseDate.minusMonths(1);
+        @Override
+        public Instant getStartInstant(Instant today, ZoneId zone) {
+            LocalDate date = LocalDate.ofInstant(today, zone)
+                .withDayOfMonth(1);
+            return date.atStartOfDay(zone).toInstant();
+        }
+
+        @Override
+        public Instant getEndInstant(Instant today, ZoneId zone) {
+            LocalDate date = LocalDate.ofInstant(today, zone)
+                .withDayOfMonth(1).plusMonths(1);
+            return date.atStartOfDay(zone).toInstant();
         }
     },
     ALL_TIME {
-        public LocalDate getStartDate(LocalDate baseDate) {
-            return LocalDate.of(2000, 1, 1);
+        @Override
+        public Instant getStartInstant(Instant today, ZoneId zone) {
+            return Instant.EPOCH; // 1970-01-01T00:00:00Z
+        }
+
+        @Override
+        public Instant getEndInstant(Instant today, ZoneId zone) {
+            return Instant.MAX;
         }
     };
 
-    public abstract LocalDate getStartDate(LocalDate baseDate);
+    public abstract Instant getStartInstant(Instant today, ZoneId zone);
+    public abstract Instant getEndInstant(Instant today, ZoneId zone);
 }
+
