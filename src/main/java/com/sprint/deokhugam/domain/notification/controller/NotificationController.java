b095037e8828d440c17d295a37eb94cd1da46fad
barrackkk <kgmlwns904@gmail.com>
2025-07-21 18:48:23 +0900

    feat: 알림 확인 시 확인 여부 PATCH 요청 구현

--- src/main/java/com/sprint/deokhugam/domain/notification/controller/NotificationController.java
+++ src/main/java/com/sprint/deokhugam/domain/notification/controller/NotificationController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +53,13 @@ public class NotificationController {
         notificationService.markAllAsRead(userId);
         return ResponseEntity.noContent().build();
     }
+
+    @PatchMapping("/{notificationId}")
+    public ResponseEntity<?> readNotifications(
+        @PathVariable(name = "notificationId") UUID id) {
+
+        notificationService.updateNotification(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
