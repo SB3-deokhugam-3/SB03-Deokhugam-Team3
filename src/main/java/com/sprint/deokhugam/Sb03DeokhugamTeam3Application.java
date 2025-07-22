266737abfd4e61a583bf38736671e8d30fc4f2db
Dw-real <gksehddn8@naver.com>
2025-07-23 02:01:32 +0900

    fix: Controller 테스트 실패하는 오류 수정

--- src/main/java/com/sprint/deokhugam/Sb03DeokhugamTeam3Application.java
+++ src/main/java/com/sprint/deokhugam/Sb03DeokhugamTeam3Application.java
@@ -1,13 +1,11 @@
 package com.sprint.deokhugam;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
-@EnableBatchProcessing
 @SpringBootApplication
+@EnableScheduling
 public class Sb03DeokhugamTeam3Application {
     public static void main(String[] args) {
         SpringApplication.run(Sb03DeokhugamTeam3Application.class, args);
