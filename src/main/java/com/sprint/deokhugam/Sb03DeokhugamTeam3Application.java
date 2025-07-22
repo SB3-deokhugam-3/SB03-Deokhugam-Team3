277f7859874960eb42caa51cb85c7971092eedf6
Dw-real <gksehddn8@naver.com>
2025-07-23 00:06:44 +0900

    feat: PopularBookRankingBatch 스케쥴러 추가

--- src/main/java/com/sprint/deokhugam/Sb03DeokhugamTeam3Application.java
+++ src/main/java/com/sprint/deokhugam/Sb03DeokhugamTeam3Application.java
@@ -1,8 +1,12 @@
 package com.sprint.deokhugam;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableBatchProcessing
 @SpringBootApplication
 public class Sb03DeokhugamTeam3Application {
     public static void main(String[] args) {
---
+++ src/main/java/com/sprint/deokhugam/domain/popularbook/scheduler/PopularBookRankingScheduler.java
@@ -0,0 +1,42 @@
+package com.sprint.deokhugam.domain.popularbook.scheduler;
+
+import com.sprint.deokhugam.global.period.PeriodType;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PopularBookRankingScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job popularBookRankingJob;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void runJob() {
+        Instant today = Instant.now();
+
+        for (PeriodType period : PeriodType.values()) {
+            try {
+                JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("period", period.name())
+                    .addString("today", today.toString())
+                    .addLong("timestamp", System.currentTimeMillis()) // 유니크 파라미터
+                    .toJobParameters();
+
+                log.info("[PopularBookRankingScheduler] 인기 도서 랭킹 배치 실행 - period: {}, today: {}", period, today);
+
+                jobLauncher.run(popularBookRankingJob, jobParameters);
+            } catch (Exception e) {
+                log.warn("[PopularBookRankingScheduler] 인기 도서 랭킹 배치 실패 : {}", e.getMessage());
+            }
+        }
+    }
+}
