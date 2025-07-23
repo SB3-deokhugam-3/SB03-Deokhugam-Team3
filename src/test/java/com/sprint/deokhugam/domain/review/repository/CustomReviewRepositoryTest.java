5869c368984605d558de2548d15e8d922dd55a57
hyohyo-zz <akbkck8101@gmail.com>
2025-07-24 00:32:23 +0900

    chore: 임포트 제거

--- src/test/java/com/sprint/deokhugam/domain/review/repository/CustomReviewRepositoryTest.java
+++ src/test/java/com/sprint/deokhugam/domain/review/repository/CustomReviewRepositoryTest.java
@@ -24,7 +24,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @Import({JpaAuditingConfig.class, QueryDslConfig.class})
