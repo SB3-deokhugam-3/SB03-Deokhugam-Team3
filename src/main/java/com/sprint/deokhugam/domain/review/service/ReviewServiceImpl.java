7fbca658945f8b2498d40158e91a5f2f37791b3d
hyohyo-zz <akbkck8101@gmail.com>
2025-07-23 19:35:45 +0900

    feat: 도서 평점 반영을 위한 reviewService에 updateRaing추가

--- src/main/java/com/sprint/deokhugam/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/sprint/deokhugam/domain/review/service/ReviewServiceImpl.java
@@ -121,6 +121,7 @@ public class ReviewServiceImpl implements ReviewService {
         Review review = new Review(rating, content, book, user);
         Review savedReview = reviewRepository.save(review);
         book.increaseReviewCount();
+        updateBookRating(request.bookId());
 
         log.info("[review] 생성 완료 - reviewId: {}, bookId: {}, userId: {}, rating: {}, content: {}",
             savedReview.getId(), bookId, userId, rating, content);
@@ -147,6 +148,7 @@ public class ReviewServiceImpl implements ReviewService {
 
         review.softDelete();
         review.getBook().decreaseReviewCount();
+        updateBookRating(review.getBook().getId());
 
     }
 
@@ -176,7 +178,21 @@ public class ReviewServiceImpl implements ReviewService {
         log.info("[review] 수정 완료 - reviewId: {}, userId: {}, newContent={}, newRating={}", reviewId,
             userId, request.content(), request.rating());
 
+        updateBookRating(review.getBook().getId());
+
         return reviewMapper.toDto(review, s3Storage);
+    }
+
+    private void updateBookRating(UUID bookId) {
+        // 도서 엔티티의 평점 업데이트
+        Book book = bookRepository.findById(bookId)
+            .orElseThrow(() -> new BookNotFoundException(bookId));
+
+        // 해당 도서의 모든 리뷰 평점 평균 계산
+        Double averageRating = reviewRepository.findAverageRatingByBook(book);
+
+        book.updateRating(averageRating != null ? averageRating : 0.0);
+        bookRepository.save(book);
     }
 
     // 검증 메서드
