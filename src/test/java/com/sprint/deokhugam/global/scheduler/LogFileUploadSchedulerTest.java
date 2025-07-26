43b2ad57ba6ad62a112086897a7bd5974be13a31
Dw-real <gksehddn8@naver.com>
2025-07-26 18:46:04 +0900

    test: S3업로드 실패 시 예외 테스트 추가

--- src/test/java/com/sprint/deokhugam/global/scheduler/LogFileUploadSchedulerTest.java
+++ src/test/java/com/sprint/deokhugam/global/scheduler/LogFileUploadSchedulerTest.java
@@ -1,5 +1,7 @@
 package com.sprint.deokhugam.global.scheduler;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.BDDAssertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,6 +11,7 @@ import static org.mockito.Mockito.verify;
 
 import com.sprint.deokhugam.global.storage.S3Storage;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
@@ -61,5 +64,24 @@ class LogFileUploadSchedulerTest {
 
         // then
         verify(s3Storage, never()).uploadFile(any());
+    }
+
+    @Test
+    void S3_업로드_실패_시_예외_처리_테스트(@TempDir Path tempDir) throws IOException {
+
+        // given
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        String fileName = String.format("app.%s.log", yesterday);
+        Path logFilePath = tempDir.resolve(fileName);
+        File logFile = logFilePath.toFile();
+
+        Files.writeString(logFile.toPath(), "테스트 로그 내용");
+        given(s3Storage.uploadFile(any(File.class))).willThrow(new RuntimeException("S3 업로드 실패"));
+
+        // when
+        Throwable thrown = catchThrowable(() -> scheduler.uploadYesterdayLogFile(tempDir.toString()));
+
+        // then
+        verify(s3Storage).uploadFile(logFile);
     }
 }
