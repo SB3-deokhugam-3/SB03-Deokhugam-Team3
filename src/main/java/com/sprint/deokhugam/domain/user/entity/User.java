8a5fe93983a0340b69ce1561ebc7ebc4b6831f71
hyohyo-zz <akbkck8101@gmail.com>
2025-07-28 11:28:02 +0900

    feat: 유저엔티티에 deletedAt 추가

--- src/main/java/com/sprint/deokhugam/domain/user/entity/User.java
+++ src/main/java/com/sprint/deokhugam/domain/user/entity/User.java
@@ -4,6 +4,7 @@ import com.sprint.deokhugam.global.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,6 +30,9 @@ public class User extends BaseUpdatableEntity {
     @Column(name = "is_deleted", nullable = false)
     private Boolean isDeleted = false;
 
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
     @Builder
     public User(String email, String nickname, String password, boolean isDeleted) {
         this.email = email;
@@ -43,8 +47,9 @@ public class User extends BaseUpdatableEntity {
         }
     }
 
-    public void deleted() {
+    public void softDelete() {
         this.isDeleted = true;
+        this.deletedAt = Instant.now();
     }
 
 }
